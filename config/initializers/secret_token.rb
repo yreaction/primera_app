36f550ca3f844dd9134df69d80fdc1dc7e8a2438
yreaction <Yreaction@iMac-de-yreaction.local>
2010-12-19 17:39:22 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PrimeraApp::Application.config.secret_token = 'a305b2a48ddf5089d2b0feff963dddb5e3c85320ff787d2573e5b2f6c544a311ee20204935ff55412a66664b63743fd3c8d13e668e55b9de55f41badaa59050b'
